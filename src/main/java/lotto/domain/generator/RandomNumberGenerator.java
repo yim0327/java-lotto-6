52c2c177c67824389481b5a76fa2699c913da6df
yim0327 <yim0327@naver.com>
2025-10-01 20:38:48 +0900

    feat(generator): RandomNumberGenerator 구현
    - RandomNumberGenerator
      - generateNumbers()에서 요구사항에 따라
        missionutils Randoms.pickUniqueNumbersInRange() 사용
      - 1~45 사이의 유니크한 숫자 6개를 리스트로 반환

--- src/main/java/lotto/domain/generator/RandomNumberGenerator.java
+++ src/main/java/lotto/domain/generator/RandomNumberGenerator.java
@@ -1,11 +1,16 @@
 package lotto.domain.generator;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class RandomNumberGenerator implements NumberGenerator {
+    private static final int MIN = 1;
+    private static final int MAX = 45;
+    private static final int COUNT = 6;
 
     @Override
     public List<Integer> generateNumbers() {
-        throw new UnsupportedOperationException("아직 구현되지 않았습니다.");
+        return List.copyOf(Randoms.pickUniqueNumbersInRange(MIN, MAX, COUNT));
     }
 }
