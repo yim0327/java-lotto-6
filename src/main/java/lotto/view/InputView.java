e370c1781eadf43a05037f2bc6292da1c2e65cf7
yim0327 <yim0327@naver.com>
2025-10-06 20:16:21 +0900

    feat(inputview): 입력값 숫자/공백 예외 기능 추가
    - valitdateNotBlank : 빈문자열/공백 예외 처리
    - valitdateIsNumeric : 입력값 숫자 검증 처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,30 +4,48 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Lotto;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class InputView {
     public int inputPrice() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
-
+        validateNotBlank(input);
+        validateIsNumeric(input);
         return Integer.parseInt(input.trim());
     }
 
     public Lotto inputWinningNum() {
         System.out.println("\n당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
-        return new Lotto(Arrays.stream(input.split(","))
+        validateNotBlank(input);
+        List<Integer> numbers = Arrays.stream(input.split(","))
                 .map(String::trim)          // 앞뒤 공백 제거
+                .peek(InputView::validateIsNumeric) // 각 토큰은 숫자여야 함
                 .map(Integer::parseInt)     // 문자열 → 숫자
-                .collect(Collectors.toList()));
+                .collect(Collectors.toList());
+        return new Lotto(numbers);
 
     }
 
     public int inputBonusNum() {
         System.out.println("\n보너스 번호를 입력해 주세요.");
         String input = Console.readLine();
+        validateNotBlank(input);
+        validateIsNumeric(input);
         return Integer.parseInt(input.trim());
     }
 
+    private void validateNotBlank(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("이름은 빈 문자열이거나 공백일 수 없습니다.");
+        }
+    }
+
+    private static void validateIsNumeric(String input) {
+        if (!input.matches("\\d+")) {
+            throw new IllegalArgumentException("숫자만 입력해야 합니다.");
+        }
+    }
 }
